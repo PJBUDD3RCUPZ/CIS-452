774a60d3e197c70bc339b0f0a6a0c482da3603ea
PJ <pjbuddercup@gmail.com>
2024-01-31 16:22:09 -0500

    Added our names to the tasks

--- Lab 3/task4.py
+++ Lab 3/task4.py
@@ -1,3 +1,6 @@
+#Created By: Kirk Caponpon, Gabe Gonzales, and Benjamin Richards
+#Lab 3
+#02/04/2024
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np 
@@ -19,6 +22,5 @@ plt.ylabel('Frequency')
 plt.title('Task 4')
 
 plt.legend()
-
 
 plt.show()
